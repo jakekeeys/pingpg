bf5c8641264ab71b4138f2a6061d00ca0ec9e569
Jake Keeys <jake@keeys.org>
2023-12-21 08:56:49 +0000

    switch to prom remote writes

--- go.mod
+++ go.mod
@@ -3,19 +3,15 @@ module github.com/jakekeeys/pingpg
 go 1.21.5
 
 require (
+	github.com/golang/protobuf v1.5.3
+	github.com/golang/snappy v0.0.4
 	github.com/prometheus-community/pro-bing v0.3.0
-	github.com/prometheus/client_golang v1.17.0
+	github.com/prometheus/prometheus v0.48.1
 )
 
 require (
-	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/uuid v1.5.0 // indirect
-	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
-	github.com/prometheus/client_model v0.4.1-0.20230718164431-9a2bf3000d16 // indirect
-	github.com/prometheus/common v0.44.0 // indirect
-	github.com/prometheus/procfs v0.11.1 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
--- main.go
+++ main.go
@@ -1,69 +1,164 @@
 package main
 
 import (
+	"bytes"
+	"context"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/snappy"
 	probing "github.com/prometheus-community/pro-bing"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/push"
+	"github.com/prometheus/prometheus/model/timestamp"
+	"github.com/prometheus/prometheus/prompb"
+
+	"net/http"
 	"os"
 	"time"
 )
 
 // TODO: discovery local gateway and publish ping metrics for it
+// TODO: detect wifi / eth
+// TODO: retry, publish historical metrics
+// TODO: impl using prom remote write
+// TODO: switch to OTEL remote write
 
 func main() {
-	registry := prometheus.NewRegistry()
-
-	labels := prometheus.Labels{}
-	minRTT := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:        "min_rtt_ms",
-		ConstLabels: labels,
-	})
-	maxRTT := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:        "max_rtt_ms",
-		ConstLabels: labels,
-	})
-	avgRTT := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:        "avg_rtt_ms",
-		ConstLabels: labels,
-	})
-	stdDevRTT := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:        "std_dev_rtt_ns",
-		ConstLabels: labels,
-	})
-	packetLoss := prometheus.NewGauge(prometheus.GaugeOpts{
-		Name:        "packet_loss",
-		ConstLabels: labels,
-	})
-
-	registry.MustRegister(minRTT, maxRTT, avgRTT, stdDevRTT, packetLoss)
-	pusher := push.New("http://pingpg.keeys.io", os.Getenv("PINGPG_CLIENTID")).Gatherer(registry).BasicAuth("pingpg", os.Getenv("PINGPG_PASS"))
-
 	for {
-		pinger, err := probing.NewPinger("www.google.com")
+		err := collectAndPush()
 		if err != nil {
 			println(err.Error())
 		}
-		pinger.Count = 10
-		pinger.SetPrivileged(true)
-
-		err = pinger.Run()
-		if err != nil {
-			println(err.Error())
-		}
-
-		stats := pinger.Statistics()
-		minRTT.Set(float64(stats.MinRtt.Milliseconds()))
-		maxRTT.Set(float64(stats.MaxRtt.Milliseconds()))
-		avgRTT.Set(float64(stats.AvgRtt.Milliseconds()))
-		stdDevRTT.Set(float64(stats.StdDevRtt.Nanoseconds()))
-		packetLoss.Set(stats.PacketLoss)
-
-		err = pusher.Push()
-		if err != nil {
-			println(err.Error())
-		}
-
 		time.Sleep(time.Second * 10)
 	}
+}
 
+func collectAndPush() error {
+	pinger, err := probing.NewPinger("1.1.1.1")
+	if err != nil {
+		return err
+	}
+	pinger.Count = 10
+	pinger.SetPrivileged(true)
+
+	err = pinger.Run()
+	if err != nil {
+		return err
+	}
+
+	now := timestamp.FromTime(time.Now().UTC())
+	var r = prompb.WriteRequest{
+		Timeseries: []prompb.TimeSeries{
+			{
+				Labels: []prompb.Label{
+					{
+						Name:  "__name__",
+						Value: "min_rtt_ms",
+					},
+					{
+						Name:  "exported_job",
+						Value: os.Getenv("PINGPG_CLIENTID"),
+					},
+				},
+				Samples: []prompb.Sample{
+					{
+						Value:     float64(pinger.Statistics().MinRtt.Milliseconds()),
+						Timestamp: now,
+					},
+				},
+			},
+			{
+				Labels: []prompb.Label{
+					{
+						Name:  "__name__",
+						Value: "max_rtt_ms",
+					},
+					{
+						Name:  "exported_job",
+						Value: os.Getenv("PINGPG_CLIENTID"),
+					},
+				},
+				Samples: []prompb.Sample{
+					{
+						Value:     float64(pinger.Statistics().MaxRtt.Milliseconds()),
+						Timestamp: now,
+					},
+				},
+			},
+			{
+				Labels: []prompb.Label{
+					{
+						Name:  "__name__",
+						Value: "avg_rtt_ms",
+					},
+					{
+						Name:  "exported_job",
+						Value: os.Getenv("PINGPG_CLIENTID"),
+					},
+				},
+				Samples: []prompb.Sample{
+					{
+						Value:     float64(pinger.Statistics().AvgRtt.Milliseconds()),
+						Timestamp: now,
+					},
+				},
+			},
+			{
+				Labels: []prompb.Label{
+					{
+						Name:  "__name__",
+						Value: "std_dev_rtt_ns",
+					},
+					{
+						Name:  "exported_job",
+						Value: os.Getenv("PINGPG_CLIENTID"),
+					},
+				},
+				Samples: []prompb.Sample{
+					{
+						Value:     float64(pinger.Statistics().StdDevRtt.Milliseconds()),
+						Timestamp: now,
+					},
+				},
+			},
+			{
+				Labels: []prompb.Label{
+					{
+						Name:  "__name__",
+						Value: "packet_loss",
+					},
+					{
+						Name:  "exported_job",
+						Value: os.Getenv("PINGPG_CLIENTID"),
+					},
+				},
+				Samples: []prompb.Sample{
+					{
+						Value:     pinger.Statistics().PacketLoss,
+						Timestamp: now,
+					},
+				},
+			},
+		},
+	}
+	pb, err := proto.Marshal(&r)
+	if err != nil {
+		return err
+	}
+
+	cb := snappy.Encode(nil, pb)
+	t, _ := context.WithTimeout(context.Background(), time.Second*3)
+	req, err := http.NewRequestWithContext(t, http.MethodPost, "http://pingpg.keeys.io/api/v1/write", bytes.NewBuffer(cb))
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Encoding", "snappy")
+	req.Header.Set("Content-Type", "application/x-protobuf")
+	req.Header.Add("User-Agent", "pingpg-v0.0.1")
+	req.Header.Add("X-Prometheus-Remote-Write-Version", "0.1.0")
+	req.SetBasicAuth("pingpg", os.Getenv("PINGPG_PASS"))
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	println(resp.Status)
+	return nil
 }
